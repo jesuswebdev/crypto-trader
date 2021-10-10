cb2544806b369f30a22da19580023ee6db67419c
Jesús Jiménez <jesuswebdev@gmail.com>
2021-10-10 17:52:57 -0400

    refactor

--- account-observer/app.js
+++ account-observer/app.js
@@ -3,7 +3,7 @@ const Observer = require("./src/observer");
 
 const start = async () => {
   try {
-    const db = await startDb();
+    const db = startDb();
     await new Observer(db).init();
   } catch (error) {
     console.error(error);
--- account-observer/config/db.js
+++ account-observer/config/db.js
@@ -2,11 +2,4 @@
 const knexfile = require("../../knexfile");
 const { ENVIRONMENT } = require("./");
 
-module.exports = async () => {
-  try {
-    const knex = require("knex")(knexfile[ENVIRONMENT]);
-    return knex;
-  } catch (error) {
-    throw error;
-  }
-};
+module.exports = () => require("knex")(knexfile[ENVIRONMENT]);
